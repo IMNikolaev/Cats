f2fef5fe9ea0839541f16a83b53954d4d7232cd5
Igor Nikolaev <tamarisk60@yandex.ru>
2024-05-07 14:48:28 +0200

    123

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,5 @@ public class Main {
         catService.newCat("Min", 1, 1);
         catService.newCat("Ok", 5, 2);
         System.out.println(catService.allCats());
-        System.out.println("Kzkzkz");
     }
 }
